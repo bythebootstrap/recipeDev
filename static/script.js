ea424842c8bb09dfbcc057d9f4636402403f6838
bythebootstrap <48031236+bythebootstrap@users.noreply.github.com>
2023-12-05 03:23:56 -0800

    basic loop listing test

--- static/script.js
+++ static/script.js
@@ -1,1 +1,14 @@
 console.log(recipes);
+
+function allRecipes(){
+    let recipeDiv = document.getElementById('recipes')
+    recipes.forEach((recipe) => {
+        let listing = document.createElement('div')
+        var categories = recipe.category.concat(', ')
+        listing.innerHTML = `
+            <a href="${recipe.slug}>${recipe.title}</a>
+            <p>${categories}</p>
+        `
+        recipeDiv.append(listing)
+    });
+}
